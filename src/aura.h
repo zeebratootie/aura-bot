e7fa429959b22107d7d1deb78624813a0a68ac2c
Ivo Julca <ivojulca@hotmail.com>
2025-08-31 05:03:26 -0500

    Version 11.3.0

--- src/aura.h
+++ src/aura.h
@@ -71,8 +71,8 @@
 #include <windows.h>
 #endif
 
-#define AURA_VERSION "11.2.1.dev"
-#define AURA_APP_NAME "Aura 11.2.1.dev"
+#define AURA_VERSION "11.3.0"
+#define AURA_APP_NAME "Aura 11.3.0"
 #define AURA_REPOSITORY_URL "https://gitlab.com/ivojulca/aura-bot"
 #define AURA_ISSUES_URL "https://gitlab.com/ivojulca/aura-bot/-/issues"
 
