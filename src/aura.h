188b158711c533d462d4d3b1de84d6a14bc444ad
Ivo Julca <ivojulca@hotmail.com>
2025-09-01 21:40:14 -0500

    Bump to v11.3.2

--- src/aura.h
+++ src/aura.h
@@ -71,8 +71,8 @@
 #include <windows.h>
 #endif
 
-#define AURA_VERSION "11.3.1"
-#define AURA_APP_NAME "Aura 11.3.1"
+#define AURA_VERSION "11.3.2"
+#define AURA_APP_NAME "Aura 11.3.2"
 #define AURA_REPOSITORY_URL "https://gitlab.com/ivojulca/aura-bot"
 #define AURA_ISSUES_URL "https://gitlab.com/ivojulca/aura-bot/-/issues"
 
